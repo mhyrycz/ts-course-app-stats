8901d140386ac241371c7a999f9f7c89b21ba047
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-07-27 14:54:59 +0200

    Parsing data from csv file.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("dupa")
+import fs from 'fs';
+
+const matches = fs
+	.readFileSync('football.csv', {
+		encoding: 'utf-8'
+	})
+	.split('\n')
+	.map((match: string): string[] => {
+		return match.split(',');
+	});
+
+console.log(matches);
