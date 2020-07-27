e37211d849458e265d509c1d763eb30a9992b28b
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-07-27 15:19:30 +0200

    Enums introduced.

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,21 @@ const matches = fs
 	.split('\n')
 	.map((match: string): string[] => {
 		return match.split(',');
-	});
+    });
+    
+
+enum MatchResult {
+    HomeWins = "H",
+    AwayWins = "A",
+    Draw = "D"
+}
 
 const manUnitedWins = (matches: string[][]): number => {
     let wins = 0
     for(let match of matches) {
-        if(match[1] === "Man United" && match[5] === "H"){
+        if(match[1] === "Man United" && match[5] === MatchResult.HomeWins){
             wins++
-        } else if(match[2] === "Man United" && match[5] === "A"){
+        } else if(match[2] === "Man United" && match[5] === MatchResult.AwayWins){
             wins++
         }
     }
@@ -22,5 +29,3 @@ const manUnitedWins = (matches: string[][]): number => {
 }
 
 console.log((manUnitedWins(matches))
-
-
