11666c8753bb06d27ff632ee0c5d22a6c8dd556d
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-07-28 19:31:30 +0200

    Generic implemented in abstract class.

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,14 +1,9 @@
 import fs from 'fs';
-import { dateStringToDate } from './utils';
-import { MatchResult } from './MatchResult';
 
-//tuple
-type MatchData = [Date, string, string, number, number, MatchResult, string];
+export abstract class CsvFileReader<TypeOfData> {
+	abstract mapRow(row: string[]): TypeOfData;
 
-export abstract class CsvFileReader {
-	abstract mapRow(row: string[]): MatchData;
-
-	data: MatchData[] = [];
+	data: TypeOfData[] = [];
 
 	constructor(public filename: string) {
 		this.read();
@@ -24,24 +19,5 @@ export abstract class CsvFileReader {
 				return match.split(',');
 			})
 			.map(this.mapRow);
-	}
-}
-
-export class MatchReader extends CsvFileReader {
-	constructor(filename: string) {
-		super(filename);
-		this.read();
-	}
-
-	mapRow(row: string[]): MatchData {
-		return [
-			dateStringToDate(row[0]),
-			row[1],
-			row[2],
-			parseInt(row[3]),
-			parseInt(row[4]),
-			row[5] as MatchResult, //Type assertion
-			row[6]
-		];
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { MatchReader } from './CsvFileReader';
+import { MatchReader, MatchData } from './MatchReader';
 import { MatchResult } from './MatchResult';
 
 const matches = new MatchReader('./football.csv');
 
-const manUnitedWins = (matches: string[][]): number => {
+const manUnitedWins = (matches: MatchData[]): string => {
 	let wins = 0;
 	for (let match of matches) {
 		if (match[1] === 'Man United' && match[5] === MatchResult.HomeWins) {
@@ -12,9 +12,8 @@ const manUnitedWins = (matches: string[][]): number => {
 			wins++;
 		}
 	}
-	return wins;
+	return `Man United won ${wins}`;
 };
 
+console.log(matches)
 console.log(manUnitedWins(matches.data));
-
-console.log(matches);
