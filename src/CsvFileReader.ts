5f6181fe24766b58e4d639a88720d911099547d9
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-07-28 15:38:58 +0200

    Type annotation added for data row as tuple.

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -2,8 +2,12 @@ import fs from 'fs';
 import { dateStringToDate } from './utils';
 import { MatchResult } from './MatchResult';
 
+//tuple
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
 export class CsvFileReader {
-	data: string[][] = [];
+	data: MatchData[] = [];
+
 	constructor(public filename: string) {
 		this.read();
 	}
@@ -17,14 +21,15 @@ export class CsvFileReader {
 			.map((match: string): string[] => {
 				return match.split(',');
 			})
-			.map((row: string[]): any => {
+			.map((row: string[]): MatchData => {
 				return [
 					dateStringToDate(row[0]),
 					row[1],
 					row[2],
 					parseInt(row[3]),
 					parseInt(row[4]),
-					row[5] as MatchResult //Type assertion
+					row[5] as MatchResult, //Type assertion
+					row[6]
 				];
 			});
 	}
