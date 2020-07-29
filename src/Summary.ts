0dbffb075c180c38ee9c01f101b770e7776b653e
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2020-07-29 15:40:07 +0200

    Static functions implemented to create new instances of class.

--- src/Summary.ts
+++ src/Summary.ts
@@ -1,4 +1,7 @@
 import { MatchData } from './MatchData';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { HtmlReport } from './reportTargets/HtmlReport';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
 
 export interface Analyzer {
 	run(matches: MatchData[]): string;
@@ -9,6 +12,14 @@ export interface OutputTarget {
 }
 
 export class Summary {
+	static withHtmlReport(team: string): Summary {
+		return new Summary(new WinsAnalysis(team), new HtmlReport());
+	}
+
+	static withConsoleReport(team: string): Summary {
+		return new Summary(new WinsAnalysis(team), new ConsoleReport());
+	}
+
 	constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {}
 
 	buildAndPrintRepor(matches: MatchData[]) {
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,10 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
 import { Summary } from './Summary';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
-import { HtmlReport } from './reportTargets/HtmlReport';
 
-const csv = new CsvFileReader('./football.csv');
+const newCSVMatches = MatchReader.withCsvFile('./football.csv');
 
-const newCSVMatches = new MatchReader(csv);
-
-const ManUnitedAnalysis = new WinsAnalysis('Everton');
-
-const newConsoleReport = new ConsoleReport();
-const newHtmlReport = new HtmlReport();
-
-const newConsoleSummary = new Summary(ManUnitedAnalysis, newConsoleReport);
-const newHtmlSummary = new Summary(ManUnitedAnalysis, newHtmlReport);
+const newConsoleSummary = Summary.withConsoleReport('Man United');
+const newHtmlSummary = Summary.withHtmlReport('Everton');
 
 newHtmlSummary.buildAndPrintRepor(newCSVMatches.matches);
 console.log(newConsoleSummary.buildAndPrintRepor(newCSVMatches.matches));
